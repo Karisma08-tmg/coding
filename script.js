c78dda79d1b14c53549bfb4bd8b8f858d09bfa3f
Karisma08-tmg <karishmatamang803@gmail.com>
2026-07-14 22:09:26 +0545

    commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let button = document.getElementById("welcomeBtn");
+button.addEventListener("click", function () {
+    alert("🎵 Welcome to Purple Playlist!\n\nEnjoy exploring your favorite artists and songs.");
 
+});
+
+document.addEventListener("visibilitychange", function () {
+    if (document.hidden) {
+        document.title = "Come back to Purple Playlist 🎵";
+    } else {
+        document.title = "Purple Playlist";
+    }
+});
